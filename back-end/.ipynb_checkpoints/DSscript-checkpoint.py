56321529fcf68b93068a2fa335721fb35780239b
Peyton Runyan <peytonrunyan@Peytons-MacBook-Pro.local>
2019-07-11 14:14:16 -0400

    formatting update

--- back-end/.ipynb_checkpoints/DSscript-checkpoint.py
+++ back-end/.ipynb_checkpoints/DSscript-checkpoint.py
@@ -16,16 +16,14 @@ import utils
 # plotly credentials
 plotly.tools.set_credentials_file(username=PLOTLY_USER_NAME, api_key=PLOTLY_API_KEY)
 
-def prepare_data(correlation_list):
+def prepare_data(lines, df_plot, correlation_list):
     """
-    Returns pandas dataframe, string with HTML formatting, pandas series, dictionary
+    Returns string with HTML formatting, pandas series, dictionary
     
     See utils.py for specifics 
     """
     
     # get data
-    lines = utils.read_in()
-    df_plot = utils.generate_df(lines)
     tweet_df = utils.tweets_df(lines)
     
     # graph captions
@@ -39,16 +37,42 @@ def prepare_data(correlation_list):
     rolling_mood = df_plot['mood'].rolling(3).mean()
     rolling_mood.fillna(0)
     
-    return df_plot, tweet_cap, caption_dict, rolling_mood
+    return tweet_cap, caption_dict, rolling_mood
 
+
+def generate_plot(column, visibility=False, col_name=False, marker_color='rgba(0, 153, 0,0.8)'):
+    """
+    Returns a plotly graph
+    """
+    if col_name:
+        name = col_name
+    else:
+        name = column.capitalize()
+    
+    trace = go.Scatter(
+        visible = visibility,
+        x = df_plot['date'],
+        y = df_plot[column],
+        mode = 'lines',
+        name = name,
+        fillcolor = marker_color,
+        fill ='tozeroy',
+        hoverinfo ='text',
+        text = df_plot[column],
+        line = dict(
+            shape='spline',
+        marker = dict(color = marker_color)) 
+    
+    return trace
+    
 def main():
     
-    # prepare data 
-    caption_dict = ['caffeine','alcohol','food','sleep','water']
-    
-    df_plot, tweet_cap, caption_dict, rolling_mood = prepare_data(caption_dict)
-
-    # create graphs to be displayed
+    lines = utils.read_in()
+    df_plot = utils.generate_df(lines)
+    correlation_list = df_plot.columns.to_list().remove('mood')
+    tweet_cap, caption_dict, rolling_mood = prepare_data(lines, df_plot, correlation_list)
+   
+    # create mood plot
     trace1 = go.Scatter(
         x = df_plot['date'],
         y = rolling_mood,
@@ -61,104 +85,25 @@ def main():
         width = 4),
         marker = dict(color='rgba(255,64,153,1)'))
 
-    trace2 = go.Scatter(
-        visible = False,
-        x = df_plot['date'],
-        y = df_plot['caffeine'],
-        mode = 'lines',
-        name= 'Caffeine',
-        text= df_plot['caffeine'],
-        fill='tozeroy',
-        fillcolor='rgba(0, 153, 0,.2)',
-        hoverinfo='text',
-        line=dict(
-            shape='spline',
-        width=0.5),
-        opacity = .01,
-        marker = dict(color = 'rgba(0, 153, 0,0.8)'))
+    data = [trace1]
+    
+    # generate plots for all other recorded items
+    for column in correlation_list:
+        data.append(generate_plot(column))
 
-    trace3 = go.Scatter(
-        visible = False,
-        x = df_plot['date'],
-        y = df_plot['alcohol'],
-        mode = 'lines',
-        name= 'Alcohol',
-        fillcolor='rgba(0, 153, 0,.2)',
-        fill='tozeroy',
-        hoverinfo = 'text',
-        text= df_plot['alcohol'],
-        line=dict(
-            shape='spline'),
-        marker = dict(color = 'rgba(0, 153, 0,0.8)'))
+    # create buttons and set visibility for each plot
+    buttons_list = []
+    for i in range(len(correlation_list)):
+        visibility_index = i+1
+        visible = ([True]+len(correlation_list)*[False])
+        visible[visibility_index] = True
 
-    trace4 = go.Scatter(
-        visible = False,
-        x = df_plot['date'],
-        y = df_plot['food'],
-        mode = 'lines',
-        name= 'Food',
-        fill='tozeroy',
-        fillcolor='rgba(0, 153, 0,.2)',
-        text= df_plot['food'],
-        line=dict(
-            shape='spline'),
-        marker = dict(color = 'rgba(0, 153, 0,0.8)'))
-
-    trace5 = go.Scatter(
-        visible = False,
-        x = df_plot['date'],
-        y = df_plot['sleep'],
-        mode = 'lines',
-        name= 'Sleep',
-        fill='tozeroy',
-        fillcolor='rgba(0, 153, 0,.2)',
-        text= df_plot['sleep'],
-        line=dict(
-            shape='spline'),
-        marker = dict(color = 'rgba(0, 153, 0,0.8)'))
-
-    trace6 = go.Scatter(
-        visible = False,
-        x = df_plot['date'],
-        y = df_plot['water'],
-        mode = 'lines',
-        name= 'Water',
-        fill='tozeroy',
-        fillcolor='rgba(0, 153, 0,.2)',
-        text= df_plot['water'],
-        line=dict(
-            shape='spline'),
-        marker = dict(color = 'rgba(0, 153, 0,0.8)'))
-
-    data = [trace1, trace2, trace3, trace4, trace5, trace6]
-
-    # create interactive menus for graphs
-    updatemenus = list([
-        dict(active=-1,
-            buttons=list([
-                dict(label = 'Caffeine',
-                    method = 'update',
-                    args = [{'visible': [True, True, False, False, False, False]},
-                            {'title': caption_dict['caffeine']}]),
-                dict(label = 'Alcohol',
-                    method = 'update',
-                    args = [{'visible': [True, False, True, False, False, False]},
-                            {'title': caption_dict['alcohol']}]),
-                dict(label = 'Food',
-                    method = 'update',
-                    args = [{'visible': [True, False, False, True, False, False]},
-                            {'title': caption_dict['food']}]),
-                dict(label = 'Sleep',
-                    method = 'update',
-                    args = [{'visible': [True, False, False, False, True, False]},
-                            {'title': caption_dict['sleep']}]),
-                dict(label = 'Water',
-                    method = 'update',
-                    args = [{'visible': [True, False, False, False, False, True]},
-                            {'title': caption_dict['water']}]),
-            ])
-        )
-    ])
+        buttons.append(dict(label = correlation_list[i].capitalize(),
+                   method = 'update',
+                   args = [{'visible': visible},
+                                {'title': caption_dict[correlation_list[i]]}]))
+        
+    updatemenus = list([{active: -1, buttons: buttons_list}])
 
     layout = dict(title = tweet_cap+'<br>Your mood over time',
                 xaxis= dict(title='Date', ticklen = 5,
@@ -170,7 +115,7 @@ def main():
                 updatemenus=updatemenus)
 
     fig = dict(data = data, layout = layout)
-    plot_url = py.plot(fig, filename='plot from API (12)', auto_open=False)
+    plot_url = py.plot(fig, filename = 'plot from API (12)', auto_open=False)
 
 #start process
 if __name__ == '__main__':
